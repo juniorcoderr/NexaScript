26c79aab88f5261e20cc9a5edec58d8c4ce3734f
Vinayak Yadav <netgoluyadav337@gmail.com>
2025-05-13 10:55:13 +0530

    Analytics added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { ClerkProvider } from "@clerk/nextjs";
 import ConvexClientProvider from "@/components/providers/ConvexClientProvider";
 import Footer from "@/components/Footer";
 import { Toaster } from "react-hot-toast";
+import { Analytics } from "@vercel/analytics/next";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -38,6 +39,7 @@ export default function RootLayout({
           <Footer />
 
           <Toaster />
+          <Analytics />
         </body>
       </html>
     </ClerkProvider>
